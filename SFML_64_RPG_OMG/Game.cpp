013283dac990e68d6c6177e449873b6923aadf4d
Steven Godbold <steven_godbold@hotmail.com>
2021-03-29 22:41:42 +0100

    Game class

---
+++ SFML_64_RPG_OMG/Game.cpp
@@ -0,0 +1,60 @@
+#include "Game.h"
+
+//Static functions
+
+//Initializer functions
+
+//Constructors/Destructors
+
+
+void Game::initWindow()
+{
+	this->window = new sf::RenderWindow(sf::VideoMode(800, 600), "C++ SFML RPG");
+
+}
+//Constructors/Destructors
+Game::Game()
+{
+	this->initWindow();
+}
+Game::~Game()
+{
+	delete this->window;
+
+}
+
+// Functions
+
+
+void Game::updateSFMLEvents()
+{
+	while (this->window->pollEvent(this->sfEvent))
+	{
+		if (this->sfEvent.type == sf::Event::Closed)
+			this->window->close();
+	}
+}
+
+void Game::update()
+{
+	this->updateSFMLEvents();
+
+}
+
+void Game::render()
+{
+	this->window->clear();
+	//Render items
+	this->window->display();
+
+}
+
+void Game::run()
+{
+	while (this->window->isOpen())
+	{
+		this->update();
+		this->render();
+
+	}
+}

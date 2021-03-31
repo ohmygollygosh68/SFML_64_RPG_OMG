4d65a8f2db3479dc71b7009b0b97f9a0b4c1bf10
Steven Godbold <steven_godbold@hotmail.com>
2021-03-31 22:58:10 +0100

    State Class

--- SFML_64_RPG_OMG/Game.h
+++ SFML_64_RPG_OMG/Game.h
@@ -1,14 +1,7 @@
 #ifndef GAME_H
 #define GAME_H
-#include<iostream>
-#include<ctime>
-#include<cstdlib>
-#include "SFML\System.hpp"
-#include "SFML\Window.hpp"
-#include "SFML\Graphics.hpp"
-#include "SFML\Audio.hpp"
-#include "SFML\Network.hpp"
 
+#include "State.h"
 
 
 class Game
